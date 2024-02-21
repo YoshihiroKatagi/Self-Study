9365a9e4b3210d89541df9559186e4d01e666813
yoshihiro.a.katagi <yoshihiro.a.katagi@rakuten.com>
2024-02-21 17:18:54 +0900

    junitbook

--- Java/JUnitJissenNyumon/tutorial/src/main/java/org/tutorial/Calculator.java
+++ Java/JUnitJissenNyumon/tutorial/src/main/java/org/tutorial/Calculator.java
@@ -1,6 +1,12 @@
 package org.tutorial;
 
 public class Calculator {
+    /**
+     * 引数で与えられた２つの値をかけ合わせた値を返す
+     * @param x 1つ目の引数
+     * @param y 2つ目の引数
+     * @return xとyをかけ合わせた値
+     */
     public int multiply(int x, int y) {
         return x * y;
     }
