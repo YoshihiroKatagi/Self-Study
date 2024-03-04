9c3ccf4e3dc3c1d24d1253fa77255b0e9ca869ca
Yoshi_K <katagiyoshihiro@gmail.com>
2024-03-04 09:11:57 +0900

    in the middle of chap17

---
+++ Java/JUnitJissenNyumon/cucumber-poker/src/test/java/cucumber/poker/PokerGameAcceptanceTest.java
@@ -0,0 +1,9 @@
+package cucumber.poker;
+
+import cucumber.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@Cucumber.Options(format = {"pretty"}, monochrome = true)
+public class PokerGameAcceptanceTest {
+}
