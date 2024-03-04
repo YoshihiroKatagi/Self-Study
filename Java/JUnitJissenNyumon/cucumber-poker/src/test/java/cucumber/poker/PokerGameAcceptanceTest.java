81b91d5de4f290a0877425ab91b317bd4a5d6d55
Yoshi_K <katagiyoshihiro@gmail.com>
2024-03-04 22:18:58 +0900

    chap17

--- Java/JUnitJissenNyumon/cucumber-poker/src/test/java/cucumber/poker/PokerGameAcceptanceTest.java
+++ Java/JUnitJissenNyumon/cucumber-poker/src/test/java/cucumber/poker/PokerGameAcceptanceTest.java
@@ -2,8 +2,22 @@ package cucumber.poker;
 
 import cucumber.junit.Cucumber;
 import org.junit.runner.RunWith;
+import cucumber.runtime.*;
 
 @RunWith(Cucumber.class)
 @Cucumber.Options(format = {"pretty"}, monochrome = true)
 public class PokerGameAcceptanceTest {
+
+    public void distributed_in_hand_S_H_D_D_C(
+            int arg1, int arg2, int arg3, int arg4, int arg5) throws Throwable {
+        throw new PendingException();
+    }
+
+    public void not_change_hand() throws Throwable {
+        throw new PendingException();
+    }
+
+    public void should_be_no_pair() throws Throwable {
+        throw new PendingException();
+    }
 }
