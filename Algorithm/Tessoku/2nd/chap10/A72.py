063c4f05c5994062bb0bad58549eab9f4a19e68d
YoshihiroKatagi <katagiyoshihiro@gmail.com>
2025-01-22 00:54:09 +0900

    chap10 complete

---
+++ Algorithm/Tessoku/2nd/chap10/A72.py
@@ -0,0 +1,30 @@
+import itertools
+
+H, W, K = map(int, input().split())
+C = [ input() for i in range(H) ]
+
+def paint_row(H, W, d,remaining_steps):
+  column = [ ([ d[i][j] for i in range(H) ].count('.'), j) for j in range(W) ]
+  column.sort()
+
+  for j in range(remaining_steps):
+    idx = column[j][1]
+    for i in range(H):
+      d[i][idx] = '#'
+  
+  return sum(map(lambda l: l.count('#'), d))
+
+
+answer = 0
+for v in itertools.product([0,1], repeat=H):
+  d = [ list(C[i]) for i in range(H) ]
+  remaining_steps = K
+  for i in range(H):
+    if v[i]== 1:
+      d[i] = [ '#' ] * W
+      remaining_steps -= 1
+  if remaining_steps >= 0:
+    subanswer = paint_row(H, W, d, remaining_steps)
+    answer = max(answer, subanswer)
+
+print(answer)
